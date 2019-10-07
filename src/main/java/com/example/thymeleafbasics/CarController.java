c4a5a9e74cf74a8a34907310bd5949c2099af76d
Darek Li <darlicki@gmail.com>
2019-10-07 13:01:15 +0200

    delete

--- src/main/java/com/example/thymeleafbasics/CarController.java
+++ src/main/java/com/example/thymeleafbasics/CarController.java
@@ -2,6 +2,7 @@ package com.example.thymeleafbasics;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,20 +15,23 @@ import java.util.List;
 public class CarController {
 
     private List<Car> cars;
-    public CarController(){
-    Car car1 = new Car("Fiat", "Bravo");
-    Car car2 = new Car("Opel", "Astra");
-    Car car3 = new Car("Ford", "Focus");
-    Car car4 = new Car("Peugeot", "308");
+
+    public CarController() {
+        Car car1 = new Car("Fiat", "Bravo");
+        Car car2 = new Car("Opel", "Astra");
+        Car car3 = new Car("Ford", "Focus");
+        Car car4 = new Car("Peugeot", "308");
 
         cars = new ArrayList<>();
         cars.add(car1);
         cars.add(car2);
         cars.add(car3);
         cars.add(car4);
-}
+
+    }
+
     @GetMapping("/car")
-    public String getCar(Model model){
+    public String getCar(Model model) {
         model.addAttribute("cars", cars);
         model.addAttribute("newCar", new Car());
         return "car";
@@ -35,10 +39,18 @@ public class CarController {
 
     //metoda na dodawanie danych
     @PostMapping("/add-car")
-    public String addCar(@ModelAttribute Car car){
+    public String addCar(@ModelAttribute Car car) {
         cars.add(car);
-       // System.out.println(car);
-       // return "car"; samo car wywala bledy nalezy przekierować
+        // System.out.println(car);
+        // return "car"; samo car wywala bledy nalezy przekierować
+        return "redirect:/car";
+    }
+
+    @DeleteMapping("/delete-car")
+    public String deleteCar(@ModelAttribute Car car) {
+     //   cars.delete(car);
+        // System.out.println(car);
+        // return "car"; samo car wywala bledy nalezy przekierować
         return "redirect:/car";
     }
 }
